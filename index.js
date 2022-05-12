a8955c2a094024f2d184ebd0a61db3dd381ecd57
AdhamWalid <Adhamwalid682@gmail.com>
2022-05-12 14:43:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1493,4 +1493,32 @@ client.on('messageCreate' , async (message) => {
     main.send(`Feedback sent at ${message.guild.name}\n> ${message.content}\nBy ${message.author}`)
   }
 })
+
+const config = {
+  "guildId": "804432734066835556",
+  "channelId": "926107320382025798",
+  "welcomeMessage": "{member} ( {memberTag} ) Welcome to **{server}**\nYou are the member number **{memberCount}**",
+  "allowBots": false,
+  "deleteTime": 60
+}
+client.on('guildMemberAdd', async (member) => {
+  if (member.guild.id != config.guildId) return;
+  if (member.user.bot && !config.allowBots) return;
+  let Wlcchannel = member.guild.channels.cache.get(config.channelId);
+  if (!Wlcchannel) return console.error("CHANNEL_NOT_FOUND: Welcome channel not found!");
+  if (config.welcomeMessage.length === 0) return console.error("NO_WELCOME_MESSAGE: Welcome message must be at least one letter!");
+  let wlcMessage = config.welcomeMessage
+      .replace(/{member}/g, `${member}`)
+      .replace(/{memberTag}/g, `${member.user.tag}`)
+      .replace(/{server}/g, `${member.guild.name}`)
+      .replace(/{memberCount}/g, `${member.guild.memberCount.toLocaleString()}`)
+      Wlcchannel.send({ content: `${wlcMessage}` }).then(async (msg) => {
+        if (config.deleteTime === 0) return;
+        setTimeout(() => msg.delete() ,(config.deleteTime * 1000))
+    })
+
+  
+});
+
+
 client.login(`OTU1NDE2OTMzODcwNzUxODI0.YjhXWw.uLmq16ApqKfQktanPdvbU6Ub0Jg`);
